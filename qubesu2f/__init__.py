fbc959971e5bad5bc5b6d763d641e67a36f756a7
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2018-11-03 04:33:55 +0100

    version 1.2.3

--- qubesu2f/__init__.py
+++ qubesu2f/__init__.py
@@ -5,5 +5,5 @@ of U2F HID device as frontend and operating one from backend.
 '''
 
 # keep this parseable by distutils.version.StrictVersion
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 __author__ = 'Wojtek Porczyk <woju@invisiblethingslab.com>'
