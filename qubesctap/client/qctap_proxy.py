QubesOS/qubes-app-u2f

commit e2288317d87ef781c26c89bc626f67363df35174

forbid circular proxying
solves QubesOS/qubes-issues/issues/9064

qubesctap/client/qctap_proxy.py
@@ -27,6 +27,7 @@ import functools
 import itertools
 import logging
 import signal
+import platform
 
 from fido2.ctap1 import APDU, ApduError, RegistrationData, SignatureData
 from fido2.ctap2 import AssertionResponse, AttestationResponse, Ctap2
@@ -39,6 +40,10 @@ from qubesctap import util
 from qubesctap.util import int_to_bytes
 
 
+class ArgumentError(RuntimeError):
+    pass
+
+
 class CTAPHIDQrexecDevice(hidemu.CTAPHIDDevice):
     """U2DHIDDevice proxied over qrexec"""
     qrexec_client = const.QREXEC_CLIENT
@@ -48,7 +53,12 @@ class CTAPHIDQrexecDevice(hidemu.CTAPHIDDevice):
         if name is None:
             name = f'Qubes OS CTAP proxy to {vmname}'
         super().__init__(name=name, **kwargs)
+        if platform.node() == vmname:
+            raise ArgumentError(
+                f"Can't proxy CTAP messages circularly {vmname}->{vmname}. "
+                "Exiting.")
         self.vmname = vmname
+
 
     async def qrexec_transaction(self, request: RequestWrapper, rpcname: str):
         """Execute one transaction over qrexec"""

commit f67e03f0cc82fddef577b044ce47fd1fdb030176

Add missing docstring
Fixes: e228831 "forbid circular proxying"

qubesctap/client/qctap_proxy.py
@@ -41,7 +41,7 @@ from qubesctap.util import int_to_bytes
 
 
 class ArgumentError(RuntimeError):
-    pass
+    """Wrong arguments"""
 
 
 class CTAPHIDQrexecDevice(hidemu.CTAPHIDDevice):